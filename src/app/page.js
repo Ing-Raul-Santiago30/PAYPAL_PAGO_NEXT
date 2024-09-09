175cab0b40f8e5c2583ba33ee911f30dbd383b20
DESKTOP-9GGKTA3\Ing Raul Santiago <ing.raul.santiago30@gmail.com>
2024-09-09 14:34:26 -0400

    segundo commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,42 @@
-"use client"
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js"
+"use client";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function HomePage() {
-  console.log(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID)
   return (
-    <div className=" h-screen bg-slate-800 flex justify-center items-center">
+    <div className="h-screen bg-slate-800 flex justify-center items-center">
       <PayPalScriptProvider
-      options={{
-        clientId: 'ASj9BEeCCS9UNGuHgr2Xp-yYAAsVaeFLIPEDwhrN9yF3W_7kmBLPkmXGbBD_fOsXnD_KmFjDKAXCsHja'
-      }}
+        options={{
+          clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID, // Usar la variable de entorno
+        }}
       >
-      
-      <PayPalButtons
-      
-       style={{
-        color: 'blue',
-        layout: 'vertical'
-       }}
-       //crear un orden de compra
-       createOrder={async ()=>{
-        // la apeticion fetch
-        const res = await fetch('/api/checkout',{
-          method: 'POST'
-        })
-        // cuando tenemos la respuesto lo convertimos ajson 
-        const order = await res.json()
-        console.log(order)
-        return order.id
-       }}
-       // ESCUCHA LA RESPUESTA QUE VENGA DESDE PAYPAL
-       onApprove={(data, acttions)=>{
-        console.log(data)
-        acttions.order.capture() // para capturar el pago 
-         
-       }}
-       onCancel={(data)=>{
-        console.log("Cancelled:", data)
-       }}
-      />
-       
+        <PayPalButtons
+          style={{
+            color: 'blue',
+            layout: 'vertical',
+          }}
+          // Crear un orden de compra
+          createOrder={async () => {
+            // La petición fetch
+            const res = await fetch('/api/checkout', {
+              method: 'POST',
+            });
+            // Cuando tenemos la respuesta, la convertimos a JSON
+            const order = await res.json();
+            console.log(order);
+            return order.id;
+          }}
+          // Escuchar la respuesta que venga desde PayPal
+          onApprove={(data, actions) => {
+            console.log(data);
+            return actions.order.capture(); // Capturar el pago
+          }}
+          onCancel={(data) => {
+            console.log("Cancelled:", data);
+          }}
+        />
       </PayPalScriptProvider>
     </div>
-  )
+  );
 }
 
-
-       
-export default HomePage
+export default HomePage;
